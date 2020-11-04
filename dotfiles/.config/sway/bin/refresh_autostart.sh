3d7c5d1b443964f65359510c640d38583008e2bb
Micke Nordin <mik@elnord.in>
2020-11-04 17:12:14 +0100

    Small fix

--- dotfiles/.config/sway/bin/refresh_autostart.sh
+++ dotfiles/.config/sway/bin/refresh_autostart.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-grep -s "Exec=" /etc/xdg/autostart/* ${HOME}/.config/autostart/* | awk -F '=' '{print "exec",$2}'| sort -u > ~/.config/sway/config.d/autostart
+BLACKLIST="gnome-keyring-daemon"
+grep -s "Exec=" /etc/xdg/autostart/* ${HOME}/.config/autostart/* | awk -F '=' '{print "exec",$2}'| sort -u | egrep -v ${BLACKLIST} > ~/.config/sway/config.d/autostart
