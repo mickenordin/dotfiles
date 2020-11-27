6b1a4c138a30e67e0ffd478d0323c2a310dc954b
Micke Nordin <mik@elnord.in>
2020-11-27 23:48:32 +0100

    Git fixes

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,5 +1,6 @@
 alias ls='exa'
 alias caldav='vdirsyncer sync && khal interactive && vdirsyncer sync && true'
+set PATH $PATH ~/.local/bin
 set TERM xterm-256color
 function fish_greeting
   status --is-login
